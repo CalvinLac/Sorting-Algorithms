f626edd4b410ead46edc6c738c75d8e77f2c836f
Calvin <Calvin.lacc@gmail.com>
2017-02-01 22:40:41 -0700

    Printing to a txt file works

--- Driver.java
+++ Driver.java
@@ -1,11 +1,29 @@
+import java.io.*;
+
 public class Driver {
 
   public static void main(String[] args) {
-    if (args.length != 2) {
-      System.out.println("Usage: java Driver <suspect input> <oracle input>");
+    if (args.length != 4) {
+      System.out.println("Usage: java Assign1 <order> <size> <algorithm> <output file>");
       System.exit(0);
     }
 
-    String 
+    String order = args[0];
+    String size = args[1];
+    String algorithm = args[2];
+    String outputFile = args[3];
+    PrintWriter printWriter = null;
+
+    try{
+    FileOutputStream file = new FileOutputStream(outputFile);
+    printWriter = new PrintWriter(file);
+    }
+
+    catch (IOException e) {
+      e.getMessage();
+    }
+
+    printWriter.println("hello");
+    printWriter.close();
   }
 }
